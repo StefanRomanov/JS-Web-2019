1992a23ad5739ff84cf138db0a3aa8d92dbe3393
stefanromanov <romanovsc2@gmail.com>
2019-02-09 10:13:14 +0200

    Added role validation top softuni wiki

--- Express.js/SoftuniWiki/config/routes.js
+++ Express.js/SoftuniWiki/config/routes.js
@@ -3,24 +3,24 @@ const restrictedPages = require('./auth');
 
 module.exports = app => {
     app.get('/', controllers.home.index);
-    app.get('/user/register', controllers.user.registerGet);
-    app.post('/user/register', controllers.user.registerPost);
-    app.get('/user/logout', controllers.user.logout);
-    app.get('/user/login', controllers.user.loginGet);
-    app.post('/user/login', controllers.user.loginPost);
+    app.get('/user/register', restrictedPages.isAnonymous ,controllers.user.registerGet);
+    app.post('/user/register', restrictedPages.isAnonymous ,controllers.user.registerPost);
+    app.get('/user/logout', restrictedPages.isAuthed, controllers.user.logout);
+    app.get('/user/login', restrictedPages.isAnonymous ,controllers.user.loginGet);
+    app.post('/user/login', restrictedPages.isAnonymous ,controllers.user.loginPost);
 
-    app.get('/article/create', controllers.article.createGet);
-    app.post('/article/create', controllers.article.createPost);
+    app.get('/article/create',restrictedPages.isAuthed, controllers.article.createGet);
+    app.post('/article/create',restrictedPages.isAuthed, controllers.article.createPost);
     app.get('/article/all', controllers.article.all);
     app.get('/article/details/:id', controllers.article.details);
-    app.get('/article/edit/:id', controllers.article.editGet);
-    app.post('/article/edit/:id', controllers.article.editPost);
-    app.get('/article/history/:id', controllers.article.history);
-    app.get('/article/lock/:id', controllers.article.lock);
-    app.get('/article/unlock/:id', controllers.article.unlock);
+    app.get('/article/edit/:id',restrictedPages.isAuthed, controllers.article.editGet);
+    app.post('/article/edit/:id',restrictedPages.isAuthed, controllers.article.editPost);
+    app.get('/article/history/:id',restrictedPages.isAuthed, controllers.article.history);
+    app.get('/article/lock/:id',restrictedPages.isAdmin, controllers.article.lock);
+    app.get('/article/unlock/:id', restrictedPages.isAdmin, controllers.article.unlock);
     app.get('/article/latest', controllers.article.latest);
 
-    app.get('/edit/:id', controllers.edit.detailsGet);
+    app.get('/edit/:id',restrictedPages.isAuthed, controllers.edit.detailsGet);
 
     app.post('/search', controllers.home.search);
 
